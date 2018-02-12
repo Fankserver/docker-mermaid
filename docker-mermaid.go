cde3960c98aff7c1e7c0f94dccbcc48ee32c52d2
Florian Kinder <florian.kinder@mcl.de>
2018-02-12 11:40:26 +0100

    Copy paste from traefik for swarm service parsing

--- docker-mermaid.go
+++ docker-mermaid.go
@@ -11,7 +11,38 @@ import (
 	"os/signal"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"net"
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/go-connections/nat"
+	"github.com/docker/docker/api/types/container"
 )
+
+// dockerData holds the need data to the Provider p
+type dockerData struct {
+	ServiceName     string
+	Name            string
+	Labels          map[string]string // List of labels set to container or service
+	NetworkSettings networkSettings
+	Health          string
+	Node            *types.ContainerNode
+}
+
+// NetworkSettings holds the networks data to the Provider p
+type networkSettings struct {
+	NetworkMode container.NetworkMode
+	Ports       nat.PortMap
+	Networks    map[string]*networkData
+}
+
+// Network holds the network data to the Provider p
+type networkData struct {
+	Name     string
+	Addr     string
+	Port     int
+	Protocol string
+	ID       string
+}
 
 func main() {
 	// subscribe to SIGINT signals
@@ -49,18 +80,25 @@ func generateHandler(w http.ResponseWriter, r *http.Request) {
 
 	text := ""
 
-	networks, err := cli.NetworkList(context.Background(), types.NetworkListOptions{})
+	networkListArgs := filters.NewArgs()
+	networkListArgs.Add("driver", "overlay")
+
+	networkList, err := cli.NetworkList(context.Background(), types.NetworkListOptions{Filters: networkListArgs})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	for _, network := range networks {
+	networkMap := make(map[string]*types.NetworkResource)
+	for _, network := range networkList {
 		text += fmt.Sprintf(
 			"network_%s{%s}\n",
 			network.ID,
 			network.Name,
 		)
+
+		networkToAdd := network
+		networkMap[network.ID] = &networkToAdd
 	}
 
 	services, err := cli.ServiceList(context.Background(), types.ServiceListOptions{})
@@ -71,20 +109,52 @@ func generateHandler(w http.ResponseWriter, r *http.Request) {
 
 
 	for _, service := range services {
+		dData := parseService(service, networkMap)
+
 		text += fmt.Sprintf(
 			"service_%s(%s)\n",
 			service.ID,
 			service.Spec.Name,
 		)
 
-		for _, network := range service.Spec.Networks {
+		for _, network := range dData.NetworkSettings.Networks {
 			text += fmt.Sprintf(
 				"service_%s --- network_%s\n",
 				service.ID,
-				network.Target,
+				network.ID,
 			)
 		}
 	}
 
 	fmt.Fprint(w, text)
+}
+
+func parseService(service swarm.Service, networkMap map[string]*types.NetworkResource) dockerData {
+	dData := dockerData{
+		ServiceName:     service.Spec.Annotations.Name,
+		Name:            service.Spec.Annotations.Name,
+		Labels:          service.Spec.Annotations.Labels,
+		NetworkSettings: networkSettings{},
+	}
+
+	if service.Spec.EndpointSpec != nil {
+		if service.Spec.EndpointSpec.Mode == swarm.ResolutionModeVIP {
+			dData.NetworkSettings.Networks = make(map[string]*networkData)
+			for _, virtualIP := range service.Endpoint.VirtualIPs {
+				networkService := networkMap[virtualIP.NetworkID]
+				if networkService != nil {
+					ip, _, _ := net.ParseCIDR(virtualIP.Addr)
+					network := &networkData{
+						Name: networkService.Name,
+						ID:   virtualIP.NetworkID,
+						Addr: ip.String(),
+					}
+					dData.NetworkSettings.Networks[network.Name] = network
+				} else {
+					logrus.Debugf("Network not found, id: %s", virtualIP.NetworkID)
+				}
+			}
+		}
+	}
+	return dData
 }
